b4bf1c0d6ccf3907d384430ffa9f0d4dcce9fad8
Axel Vestberg <axel.vestberg@gmail.com>
2018-10-16 22:27:20 +0200

    almsot done with 3.2

--- 3.2/script.js
+++ 3.2/script.js
@@ -13,4 +13,36 @@ $(document).ready(function() {
         $("#remove").remove();
     })
 
+    x = 0;
+    $("#scrollable").scroll(function() {
+        $("#scroll").text(x+=1);
+    })
+
+    y = 0;
+    $("input").keypress(function() {
+        $("#press").text(y+=1);
+    })
+
+    $("#hoverbox").hover(function(){
+        $(this).css("background", "green");
+        }, function(){
+        $(this).css("background", "red");
+    });
+
+    $("input").focus(function(){
+        $("#hoverbox").text("Focus text").fadeIn(2000);
+    });
+
+    $("#prevent").click(function(event){
+        event.preventDefault();
+    });
+
+    $(document).mousemove(function(event){ 
+        $("#position").text("X: " + event.pageX + ", Y: " + event.pageY); 
+    });
+/*
+    $("#off").click(function(){
+        $("#position").off("mousemove");
+    });
+    */
 });
